50bf608e05bac73d64d401416bb27bee563445da
Sahil Singh <kswayam2000@gmail.com>
2024-10-09 18:44:03 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Auth from "./pages/Auth"
-import Layout from "./Layout"
-import Profile from "./pages/Profile"
-
-
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Layout from "./Layout";
+import Profile from "./pages/Profile";
+import useAuthStore from "./store/authStore";
 
 function App() {
-  
+  const authUser = useAuthStore((state) => state.user);
 
   return (
     <Layout>
-     <Routes>
-      <Route path="/" element ={ <Home/>} />
-      <Route path="/auth" element ={ <Auth/>} />
-      <Route path="/:user" element ={ <Profile/>} />
-     </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/auth"} />}
+        />
+        <Route
+          path="/auth"
+          element={!authUser ? <Auth /> : <Navigate to={"/"} />}
+        />
+        <Route path="/:user" element={<Profile />} />
+      </Routes>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
