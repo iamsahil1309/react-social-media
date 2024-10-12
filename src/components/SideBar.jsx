a8872b9d52eb7596203ce493e56895b38fc76e37
Sahil Singh <kswayam2000@gmail.com>
2024-10-12 12:44:14 +0530

    sidebar updated

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,44 +1,16 @@
-import { Avatar, Button, Box, Flex, Link, Tooltip } from "@chakra-ui/react";
+import { Button, Box, Flex, Link, Tooltip } from "@chakra-ui/react";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import {
-  CreatePostLogo,
   InstagramLogo,
   InstagramMobileLogo,
-  NotificationsLogo,
-  SearchLogo,
 } from "../assets/constants";
-import { AiFillHome } from "react-icons/ai";
 import { BiLogOut } from "react-icons/bi";
 import useLogout from "../hooks/useLogout";
+import SidebarItems from "./sidebarItems/SidebarItems";
 
 const SideBar = () => {
   const { handleLogout, isLoggingOut } = useLogout();
-  const sidebarItems = [
-    {
-      icon: <AiFillHome size={25} />,
-      text: "Home",
-      link: "/",
-    },
-    {
-      icon: <SearchLogo />,
-      text: "Search",
-    },
-    {
-      icon: <NotificationsLogo />,
-      text: "Notifications",
-    },
-    {
-      icon: <CreatePostLogo />,
-      text: "Create",
-    },
-    {
-      icon: <Avatar size={"sm"} name="yugji" src="/profilepic.png" />,
-      text: "Profile",
-      link: "/asaprogrammer",
-    },
-  ];
-
   return (
     <Box
       height={"100vh"}
@@ -74,33 +46,7 @@ const SideBar = () => {
         </Link>
 
         <Flex direction={"column"} gap={5} cursor={"pointer"}>
-          {sidebarItems.map((item, index) => (
-            <Tooltip
-              key={index}
-              hasArrow
-              label={item.text}
-              placement="right"
-              ml={1}
-              openDelay={300}
-              display={{ base: "block", md: "none" }}
-            >
-              <Link
-                display={"flex"}
-                to={item.link || null}
-                as={RouterLink}
-                alignItems={"ceneter"}
-                gap={4}
-                _hover={{ bg: "whiteAlpha.400" }}
-                borderRadius={6}
-                p={2}
-                w={{ base: 10, md: "full" }}
-                justifyContent={{ base: "center", md: "flex-start" }}
-              >
-                {item.icon}
-                <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
-              </Link>
-            </Tooltip>
-          ))}
+          <SidebarItems/>
         </Flex>
         <Tooltip
           hasArrow
---
+++ src/components/sidebarItems/SidebarItems.jsx
@@ -0,0 +1,19 @@
+import CreatePost from "./CreatePost";
+import Home from "./Home";
+import Notification from "./Notification";
+// import Search from "./Search";
+import ProfileLink from "./ProfileLink";
+
+const SidebarItems = () => {
+  return (
+    <>
+      <Home />
+      {/* <Search /> */}
+      <Notification />
+      <CreatePost />
+      <ProfileLink />
+    </>
+  );
+};
+
+export default SidebarItems;
